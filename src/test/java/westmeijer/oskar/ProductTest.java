c2b065f5f2331f2fd6257717e9e267426fe441fa
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-02-10 20:47:58 +0100

    add gitigno

---
+++ src/test/java/westmeijer/oskar/ProductTest.java
@@ -0,0 +1,16 @@
+package westmeijer.oskar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProductTest {
+
+    @Test
+    public void productTest () {
+
+        Assertions.assertEquals("abc", "abc");
+
+    }
+
+
+}
