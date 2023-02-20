7fb77e5a9c0faa61b7115dfef4c7ec81118d2242
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-02-20 20:31:22 +0100

    feat: cleanup metrics

--- src/main/java/westmeijer/oskar/ProductsConsumer.java
+++ src/main/java/westmeijer/oskar/ProductsConsumer.java
@@ -1,33 +1,26 @@
 package westmeijer.oskar;
 
-import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class ProductsConsumer {
 
-    @Value(value = "${products-consumers.topic-name}")
-    private String productsTopic;
-
     private Product latestMsg;
 
-    private final Counter msgCounter;
+    private final MeterRegistry meterRegistry;
 
-    public ProductsConsumer(MeterRegistry meterRegistry) {
-        this.msgCounter = Counter.builder("products.consumed").register(meterRegistry);
-    }
 
-    // TODO: figure property injection for annotations out
-    @KafkaListener(topics = "products", containerFactory = "kafkaListenerContainerFactory")
+    @KafkaListener(topics = "${products-consumers.topic-name}", containerFactory = "kafkaListenerContainerFactory")
     public void listenToProducts(Product message) {
-        log.info("Consuming from topic: {}, message: {}", productsTopic, message);
+        log.info("Consuming from products topic, message: {}", message);
         latestMsg = message;
-        msgCounter.increment();
+        meterRegistry.counter("products.consumed").increment();
     }
 
     public Product getLatestMsg() {
--- src/test/java/westmeijer/oskar/EmbeddedKafkaIntegrationTest.java
+++ src/test/java/westmeijer/oskar/EmbeddedKafkaIntegrationTest.java
@@ -1,40 +1,47 @@
 package westmeijer.oskar;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.awaitility.Durations;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @DirtiesContext
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 public class EmbeddedKafkaIntegrationTest {
+    @Autowired
+    private ProductsProducer producer;
 
     @Autowired
     private ProductsConsumer consumer;
 
     @Autowired
-    private ProductsProducer producer;
+    private MeterRegistry meterRegistry;
 
-    @Value("${products-consumers.topic-name}")
-    private String topic;
+    @BeforeEach
+    public void init() {
+        meterRegistry.clear();
+    }
+
 
     @Test
-    public void testKafka() throws Exception {
+    public void testKafka() {
         Product p = new Product(1234, "System Design Interview");
 
         producer.sendMessage(p);
-        new CountDownLatch(1).await(5, TimeUnit.SECONDS);
-        Product consumed = consumer.getLatestMsg();
 
-        assertEquals(p, consumed);
+        await().atMost(Durations.TEN_SECONDS).untilAsserted(() -> {
+            assertEquals(p, consumer.getLatestMsg());
+            assertEquals(1d, meterRegistry.get("products.consumed").counter().count());
+        });
     }
 
 }
