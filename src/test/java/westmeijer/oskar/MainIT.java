895a63e1ddb6adc339448f2cdeb32613e5898e8b
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2025-05-30 22:32:12 +0200

    feat: add more its

--- src/test/java/westmeijer/oskar/MainIT.java
+++ src/test/java/westmeijer/oskar/MainIT.java
@@ -6,10 +6,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest
-@DirtiesContext
-@Import(EmbeddedPostgresConfig.class)
-@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
+@IntegrationTest
 public class MainIT {
 
   @Test
